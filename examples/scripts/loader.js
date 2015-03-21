548e1375c186305477b075137fe81e29dea56812
Dahmian Owen <dahmian.owen@bedrosians.com>
2015-03-21 08:12:29 -0700

    Change demo to load article through AJAX

--- examples/scripts/loader.js
+++ examples/scripts/loader.js
@@ -1,4 +1,4 @@
-var Entry = require('../templates/entry.jsx');
+var Entry = require('../templates/article.jsx');
 var React = require('react');
 
 // Isomorphic React renders twice. Once on the HTML page, once in JavaScript. If the props are the same on HTML and JavaScript, the template is synced, allowing subsequent updates without destroying user input: http://www.crmarsh.com/react-ssr/
