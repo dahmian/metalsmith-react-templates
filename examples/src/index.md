08780e528226c2ba6721168edff5e8e287f2f92e
Dahmian Owen <dahmian.owen@bedrosians.com>
2015-03-24 07:29:18 -0700

    Remove collections
    Issues with Buffers on props, and folder structure
    Address collections after more pertinent features

--- examples/src/index.md
+++ examples/src/index.md
@@ -2,3 +2,5 @@
 title: Articles
 template: list.jsx
 ---
+* [Article 1](/article1.html)
+* [Article 2](/article2.html)
--- examples/templates/list.jsx
+++ examples/templates/list.jsx
@@ -5,11 +5,7 @@ var DefaultTemplate = React.createClass({
     return <div>
       <title>{this.props.title}</title>
       <h1>{this.props.title}</h1>
-      <ul>
-        {this.props.collections.articles.map(function(element) {
-          return <li key={element.path}><a href={element.path}>{element.title}</a></li>
-        })}
-      </ul>
+      <div dangerouslySetInnerHTML={{__html: this.props.contents}} />
     </div>
   }
 });
