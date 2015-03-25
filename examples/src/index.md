bde5b6efc800876d87844a96763353cbe35eb735
Dahmian Owen <dahmian.owen@bedrosians.com>
2015-03-25 08:51:40 -0700

    List uses YAML front matter

--- examples/src/index.md
+++ examples/src/index.md
@@ -1,6 +1,9 @@
 ---
 title: Articles
 template: list.jsx
+articles:
+ - title: Article 1
+   url: article1.html
+ - title: Article 2
+   url: article2.html
 ---
-* [Article 1](/article1.html)
-* [Article 2](/article2.html)
--- examples/templates/list.jsx
+++ examples/templates/list.jsx
@@ -6,6 +6,11 @@ var DefaultTemplate = React.createClass({
       <title>{this.props.title}</title>
       <h1>{this.props.title}</h1>
       <div dangerouslySetInnerHTML={{__html: this.props.contents}} />
+      <ul>
+        {this.props.articles.map(function(value, index, array) {
+          return <li><a href={value.url}>{value.title}</a></li>
+        })}
+      </ul>
     </div>
   }
 });
